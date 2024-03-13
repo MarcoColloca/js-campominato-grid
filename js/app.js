484d3f9b46a744047307ef40fd676b6d0ba00c29
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-13 15:37:38 +0100

    Add → added class hard and condition to generating grid in order to avoid multiple grids generation

--- js/app.js
+++ js/app.js
@@ -12,23 +12,28 @@ startButtonDOMElement.addEventListener('click', function(){
     
     const gridSide = 10
     const numOfCells = gridSide ** 2 // ** significa elevato a, quindi si ottiene 10 x 10
-    
-    for(let i = 0; i < numOfCells; i++){
-        const cellNumber = i + 1;
-        // console.log(cellNumber);
-    
-        const cellDOMElement = document.createElement('div');
-        cellDOMElement.className = 'square';
-        cellDOMElement.innerHTML = cellNumber;
-    
-        gridDOMElement.appendChild(cellDOMElement)
-    
-    
-        cellDOMElement.addEventListener('click', function(){
-    
-            console.log('Hai clickato la cella numero ', cellNumber)
-    
-            cellDOMElement.classList.toggle('bg-azure')
-        })
+    console.log(gridDOMElement.className)
+    if(gridDOMElement.className === ''){
+
+        for(let i = 0; i < numOfCells; i++){
+            const cellNumber = i + 1;
+            // console.log(cellNumber);
+        
+            const cellDOMElement = document.createElement('div');
+            cellDOMElement.className = 'square';
+            cellDOMElement.innerHTML = cellNumber;
+        
+            gridDOMElement.appendChild(cellDOMElement)
+        
+        
+            cellDOMElement.addEventListener('click', function(){
+        
+                console.log('Hai clickato la cella numero ', cellNumber)
+        
+                cellDOMElement.classList.toggle('bg-azure')
+            })
+        }
     }
+    gridDOMElement.classList.add('hard')
+    console.log(gridDOMElement.className)
 })
