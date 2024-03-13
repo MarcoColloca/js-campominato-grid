f5cac0027aaf08be653d301256d6568d88dc1c30
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-13 15:01:23 +0100

    Add → added dynamic JavaScript Generated 10x10 grid

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('JS Trial')
+
+const gridDOMElement = document.getElementById('grid')
+// console.log(gridDOMElement)
+
+const gridSide = 10
+const numOfCells = gridSide ** 2 // ** significa elevato a, quindi si ottiene 10 x 10
+
+for(let i = 0; i < numOfCells; i++){
+    const cellNumber = i + 1;
+    console.log(cellNumber);
+
+    const cellDOMElement = document.createElement('div');
+    cellDOMElement.className = 'square';
+    cellDOMElement.innerHTML = cellNumber;
+
+    gridDOMElement.appendChild(cellDOMElement)
+}
