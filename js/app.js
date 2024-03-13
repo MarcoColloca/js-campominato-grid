bf0778fd88f5b194b53742e86f75f93eea305a09
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-13 18:42:03 +0100

    Refactor → refactored working JavaScript logic

--- js/app.js
+++ js/app.js
@@ -4,90 +4,46 @@ console.log('JS Trial')
 /// ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ Mie Funzioni ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ \\\
 
 function gameClickStart(){
+
+    let gridSide = parseInt(difficultySelectorDOMElement.value); //number
+    const numOfCells = gridSide ** 2 // number → ** significa elevato a, quindi si ottiene 10 x 10    
+
+    for(let i = 0; i < numOfCells; i++){
+
+        const cellDOMElement = document.createElement('div');
+        let divClass = 'square-easy' //string
+
+        const cellNumber = i + 1; //number
+        cellDOMElement.innerHTML = cellNumber; //string
+
+        if(difficultySelectorDOMElement.value === '10'){
+     
+            divClass = 'square-hard'; //string
+        
+        } else if(difficultySelectorDOMElement.value === '9'){
+      
+            divClass = 'square-medium'; //string
+           
+        } 
+
+        cellDOMElement.classList.add(divClass) //object
+        gridDOMElement.appendChild(cellDOMElement) ////object
+
+        cellDOMElement.addEventListener('click', function(){
+        
+            console.log('Hai clickato la cella numero ', cellNumber)
     
-    
-    // console.log(gridDOMElement.className)
-    if(difficultySelectorDOMElement.value === 'hard'){
-
-        const hardGridSide = 10;
-        const numOfCells = hardGridSide ** 2 // ** significa elevato a, quindi si ottiene 10 x 10    
-    
-        for(let i = 0; i < numOfCells; i++){
-            const cellNumber = i + 1;
-            // console.log(cellNumber);
-        
-            const cellDOMElement = document.createElement('div');
-            cellDOMElement.className = 'square-hard';
-            cellDOMElement.innerHTML = cellNumber;
-        
-            gridDOMElement.appendChild(cellDOMElement)
-        
-        
-            cellDOMElement.addEventListener('click', function(){
-        
-                console.log('Hai clickato la cella numero ', cellNumber)
-        
-                cellDOMElement.classList.toggle('bg-azure')
-            })
-        }
-
-    } else if(difficultySelectorDOMElement.value === 'medium'){
-
-        const mediumGridSide = 9;
-        const numOfCells = mediumGridSide ** 2 // ** significa elevato a, quindi si ottiene 9 x 9    
-
-        for(let i = 0; i < numOfCells; i++){
-            const cellNumber = i + 1;
-            // console.log(cellNumber);
-        
-            const cellDOMElement = document.createElement('div');
-            cellDOMElement.className = 'square-medium';
-            cellDOMElement.innerHTML = cellNumber;
-        
-            gridDOMElement.appendChild(cellDOMElement)
-        
-        
-            cellDOMElement.addEventListener('click', function(){
-        
-                console.log('Hai clickato la cella numero ', cellNumber)
-        
-                cellDOMElement.classList.toggle('bg-azure')
-            })
-        }
-
-    } else if (difficultySelectorDOMElement.value === 'easy'){
-
-        const easyGridSide = 7;
-        const numOfCells = easyGridSide ** 2 // ** significa elevato a, quindi si ottiene 7 x 7    
-
-        for(let i = 0; i < numOfCells; i++){
-            const cellNumber = i + 1;
-            // console.log(cellNumber);
-        
-            const cellDOMElement = document.createElement('div');
-            cellDOMElement.className = 'square-easy';
-            cellDOMElement.innerHTML = cellNumber;
-        
-            gridDOMElement.appendChild(cellDOMElement)
-        
-        
-            cellDOMElement.addEventListener('click', function(){
-        
-                console.log('Hai clickato la cella numero ', cellNumber)
-        
-                cellDOMElement.classList.toggle('bg-azure')
-            })
-        }
+            cellDOMElement.classList.toggle('bg-azure') //object
+        })
     }
 
     startButtonDOMElement.removeEventListener('click', gameClickStart);
-    //console.log(gridDOMElement.className)
 }
 
 
 function gameClickReset(){
 
-    gridDOMElement.innerHTML= '';
+    gridDOMElement.innerHTML= ''; //string
     startButtonDOMElement.addEventListener('click', gameClickStart);
 
 }
@@ -100,18 +56,17 @@ function gameClickReset(){
 
 
 
-const gridDOMElement = document.getElementById('grid'); // object
-// console.log(gridDOMElement)
 
-const startButtonDOMElement = document.getElementById('generate-grid'); // object
-// console.log(startButtonDOMElement);
 
-const difficultySelectorDOMElement = document.getElementById('difficulty-selection')
-// console.log(difficultySelectorDOMElement)
-// console.log(difficultySelectorDOMElement.value)
 
-const restartButtonDOMElement = document.getElementById('delete-grid')
-// console.log(restartButtonDOMElement)
+
+const gridDOMElement = document.getElementById('grid'); //object
+
+const startButtonDOMElement = document.getElementById('generate-grid'); //object
+
+const difficultySelectorDOMElement = document.getElementById('difficulty-selection') //object
+
+const restartButtonDOMElement = document.getElementById('delete-grid') //object
 
 
 
