d607922ee05638e85ec6ab2cba5c7bac5fdf6349
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-13 15:20:47 +0100

    Feat → added button to generate 10x10 grid

--- js/app.js
+++ js/app.js
@@ -1,26 +1,34 @@
 console.log('JS Trial')
 
-const gridDOMElement = document.getElementById('grid')
+const gridDOMElement = document.getElementById('grid'); // object
 // console.log(gridDOMElement)
 
-const gridSide = 10
-const numOfCells = gridSide ** 2 // ** significa elevato a, quindi si ottiene 10 x 10
-
-for(let i = 0; i < numOfCells; i++){
-    const cellNumber = i + 1;
-    console.log(cellNumber);
-
-    const cellDOMElement = document.createElement('div');
-    cellDOMElement.className = 'square';
-    cellDOMElement.innerHTML = cellNumber;
-
-    gridDOMElement.appendChild(cellDOMElement)
+const startButtonDOMElement = document.getElementById('generate-grid'); // object
+console.log(startButtonDOMElement);
 
 
-    cellDOMElement.addEventListener('click', function(){
 
-        console.log('Hai clickato la cella numero ', cellNumber)
-
-        cellDOMElement.classList.toggle('bg-azure')
-    })
-}
+startButtonDOMElement.addEventListener('click', function(){
+    
+    const gridSide = 10
+    const numOfCells = gridSide ** 2 // ** significa elevato a, quindi si ottiene 10 x 10
+    
+    for(let i = 0; i < numOfCells; i++){
+        const cellNumber = i + 1;
+        // console.log(cellNumber);
+    
+        const cellDOMElement = document.createElement('div');
+        cellDOMElement.className = 'square';
+        cellDOMElement.innerHTML = cellNumber;
+    
+        gridDOMElement.appendChild(cellDOMElement)
+    
+    
+        cellDOMElement.addEventListener('click', function(){
+    
+            console.log('Hai clickato la cella numero ', cellNumber)
+    
+            cellDOMElement.classList.toggle('bg-azure')
+        })
+    }
+})
